fcfca773a7fffc956f24721fe3a8c38516d90022
raweber <99535319+raweber42@users.noreply.github.com>
2022-02-19 19:21:39 +0100

    First try, not working yet

---
+++ sudokulike.c
@@ -0,0 +1,189 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   backtrack.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/19 11:01:08 by coder             #+#    #+#             */
+/*   Updated: 2022/02/19 16:21:48 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void ft_pre_processing(int array[5][6], int total_size)
+{
+	int row;
+	int col;
+	int min = 1;
+	int max = total_size - 2; //because we have 5 instead of 3
+	
+	while (row < total_size)
+	{
+		while (col < total_size)
+		{
+			// //if (row != 0 && col != 0)
+			// printf("%d", array[row][col]);
+			// if (row == 0 || row == 4)
+			// 	col++;
+			// else
+			// 	col+=3;
+
+			// deal with 0th and 4th (last) row
+			if (row == 0 && array[row][col] == max)
+			{
+				array[row + 1][col] = min; // while loop instead
+				array[row + 2][col] = min + 1;
+				array[row + 3][col] = min + 2;
+			}
+			if (row == 0 && array[row][col] == min)
+				array[row + 1][col] = max;
+
+			if (row == 4 && array[row][col] == max)
+			{
+				array[row - 1][col] = min; // while loop instead
+				array[row - 2][col] = min + 1;
+				array[row - 3][col] = min + 2;
+			}
+			if (row == 4 && array[row][col] == min)
+				array[row - 1][col] = max;
+
+			if (col == 0 && array[row][col] == max)
+			{
+				array[row][col + 1] = min; // while loop instead
+				array[row][col + 2] = min + 1;
+				array[row][col + 3] = min + 2;
+			}
+			if (col == 0 && array[row][col] == min)
+					array[row][col + 1] = max;
+			
+			if (col == 4 && array[row][col] == max)
+			{
+					array[row][col - 1] = min; // while loop instead
+					array[row][col - 2] = min + 1;
+					array[row][col - 3] = min + 2;
+			}
+			if (col == 4 && array[row][col] == min)
+					array[row][col - 1] = max;
+			col++;
+		}
+		col = 0;
+		row++;
+	}
+}
+
+int ft_valid_placement(int row, int col, int array[5][6])
+{
+	
+}
+
+// MAGIC START ###############
+int	ft_solve(int row, int col, int array[5][6]))
+{
+	int i;
+
+	//i = 1; // go into actual array -> implemented in "else"
+	if (row < 4 && col < 4)
+	{
+		if (array[row][col] != 0)
+		{
+			if ((col + 1) < 4) // next column
+				return ft_solve(row, col + 1, array);
+			else if ((row + 1) < 4) // next row
+				return ft_solve(row + 1, 1, array);
+			else 
+				return 1; // solved!
+		}
+		else
+		{
+			i = 1;
+			while (i < 4)
+			{
+				if (ft_valid_placement(row, col, array, i)) // why did he use i+1?
+				{
+					array[row][col] = i;
+					if ((col + 1) < 4)
+					{
+						if (ft_solve(row, col + 1, array))
+							return 1;
+						else
+							array[row][col] = 0;
+					}
+					else if ((row + 1) < 4)
+					{
+						if (ft_solve(row + 1, 0, array))
+							return 1;
+						else
+							array[row][col] = 0;
+					}
+					else
+						return 1;
+				}
+				i++;
+			}
+		}
+		return 0;
+	}
+	else
+		return 1;
+
+}
+
+// MAGIC END ##############
+
+
+// this should take command line arguments! // convert to to integers, they are chars!!!!
+int main(void)
+{
+	int total_size;
+	int row;
+	int col;
+	int max;
+	int min;
+	int temp;
+	
+	row = 0;
+	col = 0;
+	total_size = 5;
+	max = 3;
+	min = 1;
+	int	array[5][6] =   {{0, 1, 2, 3, 0},
+						 {1, 0, 0, 0, 3},
+						 {2, 0, 0, 0, 2},
+						 {2, 0, 0, 0, 1},
+						 {0, 2, 2, 1, 0}};
+	
+	ft_pre_processing(array, total_size);
+
+	// here goes the magic! (only for 3x3 array!) --> check this again!
+	row = 1;
+	col = 1;
+	while (row < total_size - 1)
+		{
+		while (col < total_size - 1)
+		{
+			//magic backtracking
+			ft_solve(row, col, array, total_size);
+			col++;
+		}
+		row++;
+		col = 1;
+	}
+
+	// print the WHOLE (incl outer columns) array after solving it.
+	row = 0;
+	col = 0;
+	while (row < total_size)
+		{
+		while (col < total_size)
+		{
+			printf("%d", array[row][col]);
+			col++;
+		}
+		row++;
+		col = 0;
+		printf("\n");
+	}
+	return 0;
+}
